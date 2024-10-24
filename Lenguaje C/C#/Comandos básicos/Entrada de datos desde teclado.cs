4c79c7c71afdc61c3912390e3e9dd9f1b9e915eb
EmmanuelN <ednavas2023@gmail.com>
2024-10-24 11:56:34 -0500

    Create Entrada de datos desde teclado.cs

---
+++ Lenguaje C/C#/Comandos básicos/Entrada de datos desde teclado.cs
@@ -0,0 +1,7 @@
+// Leer un número entero
+Console.Write("Ingrese un número: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+// Leer un string
+Console.Write("Ingrese su nombre: ");
+string nombre = Console.ReadLine();
