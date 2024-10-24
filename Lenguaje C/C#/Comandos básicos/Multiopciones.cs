32a514471ba8a962a6f073f1284d7e1f5a83d63f
EmmanuelN <ednavas2023@gmail.com>
2024-10-24 12:01:00 -0500

    Create Multiopciones.cs

---
+++ Lenguaje C/C#/Comandos básicos/Multiopciones.cs
@@ -0,0 +1,16 @@
+int dia = 3;
+switch (dia)
+{
+    case 1:
+        Console.WriteLine("Lunes");
+        break;
+    case 2:
+        Console.WriteLine("Martes");
+        break;
+    case 3:
+        Console.WriteLine("Miércoles");
+        break;
+    default:
+        Console.WriteLine("Día no válido.");
+        break;
+}
