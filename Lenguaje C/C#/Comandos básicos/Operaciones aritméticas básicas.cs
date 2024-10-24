36d10ae933cd92065d029ed1c00c7584ea26fb84
EmmanuelN <ednavas2023@gmail.com>
2024-10-24 11:57:06 -0500

    Create Operaciones aritméticas básicas.cs

---
+++ Lenguaje C/C#/Comandos básicos/Operaciones aritméticas básicas.cs
@@ -0,0 +1,7 @@
+int a = 5;
+int b = 3;
+int suma = a + b;
+int resta = a - b;
+int multiplicacion = a * b;
+double division = (double)a / b;  // Convertimos para división decimal
+Console.WriteLine($"Suma: {suma}, Resta: {resta}, Multiplicación: {multiplicacion}, División: {division}");
