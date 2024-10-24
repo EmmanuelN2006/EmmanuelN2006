97d7aa4a5e7ada0aaa16654cc494ebfe91d27d79
EmmanuelN <ednavas2023@gmail.com>
2024-10-24 12:06:24 -0500

    Create Uso de break y continue en ciclos.cs

---
+++ Lenguaje C/C#/Comandos básicos/Uso de break y continue en ciclos.cs
@@ -0,0 +1,15 @@
+//Uso del Break
+for (int i = 0; i < 10; i++)
+{
+    if (i == 5)
+        break;  // Sale del ciclo cuando i es 5
+    Console.WriteLine(i);
+}
+
+//Uso del continue
+for (int i = 0; i < 10; i++)
+{
+    if (i == 5)
+        continue;  // Salta el resto del código cuando i es 5
+    Console.WriteLine(i);
+}
