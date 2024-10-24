bb553b4ddfc0f1470a42fd8d9497909b1224870e
EmmanuelN <ednavas2023@gmail.com>
2024-10-24 12:01:47 -0500

    Create Operadores lógicos.cs

---
+++ Lenguaje C/C#/Comandos básicos/Operadores lógicos.cs
@@ -0,0 +1,13 @@
+//AND (&&), OR (||), NOT (!)
+
+bool esMayorDeEdad = true;
+bool tieneLicencia = false;
+
+if (esMayorDeEdad && tieneLicencia)
+{
+    Console.WriteLine("Puedes conducir.");
+}
+else
+{
+    Console.WriteLine("No puedes conducir.");
+}
