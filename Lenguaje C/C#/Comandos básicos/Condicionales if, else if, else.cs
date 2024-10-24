5c53fe12f38459ebc6d80d522e1c349542d091ec
EmmanuelN <ednavas2023@gmail.com>
2024-10-24 11:57:34 -0500

    Create Condicionales if, else if, else.cs

---
+++ Lenguaje C/C#/Comandos básicos/Condicionales if, else if, else.cs
@@ -0,0 +1,13 @@
+int edad = 20;
+if (edad >= 18)
+{
+    Console.WriteLine("Eres mayor de edad.");
+}
+else if (edad >= 13)
+{
+    Console.WriteLine("Eres adolescente.");
+}
+else
+{
+    Console.WriteLine("Eres niño.");
+}
