4207757402ad0dedf94b8213747195be78c019f1
EmmanuelN <ednavas2023@gmail.com>
2024-10-24 12:00:42 -0500

    Create Conversiones de variables.cs

---
+++ Lenguaje C/C#/Comandos básicos/Conversiones de variables.cs
@@ -0,0 +1,8 @@
+string numeroComoTexto = "123";
+int numero = Convert.ToInt32(numeroComoTexto); //De string a entero
+
+int numero = 123;
+string numeroComoTexto = numero.ToString(); //De entero a string
+
+string decimalComoTexto = "3.14";
+double numeroDecimal = Convert.ToDouble(decimalComoTexto); //De string a float (double)
