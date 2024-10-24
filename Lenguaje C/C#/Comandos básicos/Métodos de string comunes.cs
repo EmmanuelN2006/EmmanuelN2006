628d25afd7608e969021100ad3bc7a8556f289af
EmmanuelN <ednavas2023@gmail.com>
2024-10-24 12:06:49 -0500

    Create Métodos de string comunes.cs

---
+++ Lenguaje C/C#/Comandos básicos/Métodos de string comunes.cs
@@ -0,0 +1,13 @@
+string frase = "Hola Mundo";
+
+// Longitud de la cadena
+Console.WriteLine(frase.Length);
+
+// Convertir a mayúsculas
+Console.WriteLine(frase.ToUpper());
+
+// Subcadena
+Console.WriteLine(frase.Substring(0, 4));  // "Hola"
+
+// Reemplazar texto
+Console.WriteLine(frase.Replace("Mundo", "C#"));  // "Hola C#"
