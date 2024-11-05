9e6ba30f51258dca74480a85e36e47f833c9d389
EmmanuelN <ednavas2023@gmail.com>
2024-11-05 13:14:46 -0500

    Create Inicio de un videjuego.py

---
+++ Python/Proyectos/Ejemplos de proyectos/Videojuego/Funciones importantes para hacer juegos en Python/Inicio de un videjuego.py
@@ -0,0 +1,8 @@
+#Descargamos la librería para usarla en el código, para eso vete al apartado de la terminal y escribe:
+#pip install pygame
+
+import pygame #Incluimos la librería en el código
+#import pygame as py #Si usamos el comando as, podremos abreviar los comandos de pygame -> py
+
+pygame.init() #Inicia la configuración del juego
+###Comandos puestos abajo serán guardados en el juego###
