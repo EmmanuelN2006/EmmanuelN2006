408c1073119e4fd70d227adeedddd110861f71a5
EmmanuelN <ednavas2023@gmail.com>
2024-11-07 10:44:13 -0500

    Update Script_modelo.py

--- Python/Proyectos/Ejemplos de proyectos/Videojuego/Script_modelo.py
+++ Python/Proyectos/Ejemplos de proyectos/Videojuego/Script_modelo.py
@@ -1,38 +1,39 @@
 #Código del videojuego: https://github.com/EmmanuelN2006/EmmanuelN2006/tree/main/Python/Proyectos/Ejemplos%20de%20proyectos/Videojuego
-import pygame
-import random 
+import pygame #pip install pygame
+import random #pip install random
 
-pygame.init()
+pygame.init() #Inicia la configuración del videojuego
 
-Finalizar = False
+Finalizar = False #Condición para cerrarlo
 
 #Configuración de la pantalla
 BLANCO = (0xFF, 0xFF, 0xFF)
 Dimensiones = [720,480]
 Ventana_principal = pygame.display.set_mode(Dimensiones); pygame.display.set_caption("Proyecto - Videojuego")
+#Configura el tamaño de la ventana y el nombre de la ventana
 
 #Configuración del jugador
 Imagen_jugador = pygame.image.load('Proyecto de estructuras - Videojuego\Personaje_ejemplo2.png')
 #Imagen_jugador = pygame.image.load('Proyecto de estructuras - Videojuego\Personaje_ejemplo.png') si quiere usar otra imagen
 #Recordar que la imagen debe estar dentro del area de trabajo y deben dar click derecho encima de la imagen y darle a
 #'Copia de ruta de acceso relativo
-x = 720/2; y = 480/2; Posicion_jugador = [x,y]
-Velocidad_jugadorx = float; Velocidad_jugadorx = 1.7
-Velocidad_jugadory = float; Velocidad_jugadory = 1.7
+x = 720/2; y = 480/2; Posicion_jugador = [x,y] #Posición inicial del jugador
+Velocidad_jugadorx = float; Velocidad_jugadorx = 1.7 #Velocidad en x
+Velocidad_jugadory = float; Velocidad_jugadory = 1.7 #Velocidad en y
 
 #Configuración de la velocidad y frames
-reloj = pygame.time.Clock()
+reloj = pygame.time.Clock() #Importante en la actualización de imágenes
 
 ##Bucle##
 while not Finalizar:
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
+    for evento in pygame.event.get(): 
+        if evento.type == pygame.QUIT: #Cierra el juego cuando se usa el cerrar ventana
             Finalizar = True
             print("Se cerró el juego") 
             pygame.quit()
     ###
     Tecla_presionada = pygame.key.get_pressed()
-    if Tecla_presionada[pygame.K_ESCAPE]:
+    if Tecla_presionada[pygame.K_ESCAPE]: #Cierra el juego cuando le da a la tecla escape
         print("Se cerró el juego") 
         exit()
     ### Comprobación de situaciones ##
@@ -97,3 +98,18 @@ while not Finalizar:
 ########################################################################################
 pygame.quit() #Si no hay otra acción a realizar - Cerrar el juego
 print("Se cerró el juego") 
+
+#Objetivos del proyecto
+#1. Establecer la idea principal - Hecho: Videojuego
+#2. Dar un enfoque especial al proyecto para que no parezca una simple copia - 
+#3. Conocer las librerías que se planean utilizar para el desarrollo del mismo -
+#4. Empezar con la estructura básica... -
+    #La estructura básica para hacer el juego es:
+    #El boceto inicial del juego y sus sprites - Hecho
+    #Movimiento del jugador - Hecho
+    #Los sprites del juego -
+    #Los scripts del juego - Hecho
+    #La interfaz del juego -
+    #La jugabilidad del juego -
+    #La música del juego -
+#5. Planeación de la exposición del juego -
