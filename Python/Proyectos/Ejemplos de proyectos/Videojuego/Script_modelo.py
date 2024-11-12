c94200e682bd426097c3e65c91601ddd1c45102b
EmmanuelN <ednavas2023@gmail.com>
2024-11-12 09:11:19 -0500

    Update Script_modelo.py

--- Python/Proyectos/Ejemplos de proyectos/Videojuego/Script_modelo.py
+++ Python/Proyectos/Ejemplos de proyectos/Videojuego/Script_modelo.py
@@ -14,6 +14,7 @@ Ventana_principal = pygame.display.set_mode(Dimensiones); pygame.display.set_cap
 
 #Configuración del jugador
 Imagen_jugador = pygame.image.load('Proyecto de estructuras - Videojuego\Personaje_ejemplo2.png')
+#Imagen_velozjugador = pygame.image.load('Proyecto de estructuras - Videojuego\Personaje_ejemplo.png') - Animación de acelerar
 #Imagen_jugador = pygame.image.load('Proyecto de estructuras - Videojuego\Personaje_ejemplo.png') si quiere usar otra imagen
 #Recordar que la imagen debe estar dentro del area de trabajo y deben dar click derecho encima de la imagen y darle a
 #'Copia de ruta de acceso relativo
@@ -31,65 +32,164 @@ while not Finalizar:
             Finalizar = True
             print("Se cerró el juego") 
             pygame.quit()
-    ###
+    ### Cierre alternativo ###
     Tecla_presionada = pygame.key.get_pressed()
     if Tecla_presionada[pygame.K_ESCAPE]: #Cierra el juego cuando le da a la tecla escape
         print("Se cerró el juego") 
         exit()
-    ### Comprobación de situaciones ##
-    if Tecla_presionada[pygame.K_LSHIFT]:  #Opción de correr con el L_Shift o el shift izquierdo 
-        Correr = bool; Correr = True
-        while Correr == True: #Cuando el shift lo hayan presionado, correra la distancia 1 * 1.7
-            if Tecla_presionada[pygame.K_UP]:
-                Arriba = y - 1* Velocidad_jugadory #Agrega una variación mayor para tener velocidad
-                y = Arriba #Obliga a la variable y a ser igual al valor de Arriba
-                Posicion_jugador = [x, y]
-                # print(y)
-            elif Tecla_presionada[pygame.K_LEFT]:
-                Izquierda = x - 1* Velocidad_jugadorx #Agrega una variación mayor para tener velocidad
-                x = Izquierda #Obliga a la variable x a hacer igual de Izquierda
-                Posicion_jugador = [Izquierda, y]
-                # print(x)
-            elif Tecla_presionada[pygame.K_RIGHT]:
-                Derecha = x + 1* Velocidad_jugadorx #Agrega una variación mayor para tener velocidad
-                x = Derecha #Obliga a la variable x a hacer igual de Derecha
-                Posicion_jugador = [Derecha, y]
-                # print(x)
-            elif Tecla_presionada[pygame.K_DOWN]:
-                Abajo = y + 1* Velocidad_jugadory #Agrega una variación mayor para tener velocidad
-                y = Abajo #Obliga a la variable y a ser igual al valor de Abajo
-                Posicion_jugador = [x, Abajo]
-                # print(y)
-            if Tecla_presionada[pygame.K_LSHIFT]: #Cuando se deja de presionar el shift izquierdo, el L-shift
-                Correr = False
-    else: #Cuando no quiere correr
-        if Tecla_presionada[pygame.K_UP]:
-            Arriba = y - 1
+    ### Comprobación de situaciones ###
+    ##Cuando el jugador se está moviendo o quiere moverse ###
+    else: # Cuando no quiere correr #
+        if Tecla_presionada[pygame.K_g]: #Centra al jugador si es que se pierde
+            print("Centrando jugador")
+            Centro = 360; Centro2 = 240
+            x = Centro; y = Centro2
+            Posicion_jugador = [x, y]
+        ###Configuración con controles duales flecha arriba-izquierda ejemplo
+        if Tecla_presionada[pygame.K_UP] and Tecla_presionada[pygame.K_RIGHT]: #Diagonal arriba derecha
+            Derecha = x + 1.5
+            x = Derecha
+            Arriba = y - 1.5
+            y = Arriba
+            Posicion_jugador = [x, y]
+        if Tecla_presionada[pygame.K_UP] and Tecla_presionada[pygame.K_LEFT]: #Diagonal arriba izquierda
+            Izquierda = x - 1.5
+            x = Izquierda
+            Arriba = y - 1.5
+            y = Arriba
+            Posicion_jugador = [x, y] 
+        if Tecla_presionada[pygame.K_DOWN] and Tecla_presionada[pygame.K_RIGHT]: #Diagonal abajo derecha
+            Derecha = x + 1.5
+            x = Derecha
+            Abajo = y + 1.5
+            y = Abajo
+            Posicion_jugador = [x, y]
+        if Tecla_presionada[pygame.K_DOWN] and Tecla_presionada[pygame.K_LEFT]: #Diagonal abajo izquierda
+            Izquierda = x - 2
+            x = Izquierda
+            Abajo = y + 2
+            y = Abajo
+            Posicion_jugador = [x, y]
+        ###Configuración con controles duales flecha W-A ejemplo
+        if Tecla_presionada[pygame.K_w] and Tecla_presionada[pygame.K_d]: #Diagonal arriba derecha
+            Arriba = y - 1.5
+            y = Arriba
+            Derecha = x + 1.5
+            x = Derecha
+            Posicion_jugador = [x, y]
+        if Tecla_presionada[pygame.K_w] and Tecla_presionada[pygame.K_a]: #Diagonal arriba izquierda
+            Arriba = y - 1.5
+            y = Arriba
+            Izquierda = x - 1.5
+            x = Izquierda
+            Posicion_jugador = [x, y] 
+        if Tecla_presionada[pygame.K_s] and Tecla_presionada[pygame.K_d]: #Diagonal abajo derecha
+            Abajo = y + 1.5
+            y = Abajo
+            Derecha = x + 1.5
+            x = Derecha
+            Posicion_jugador = [x, y]
+        if Tecla_presionada[pygame.K_s] and Tecla_presionada[pygame.K_a]: #Diagonal abajo izquierda
+            Abajo = y + 2
+            y = Abajo
+            Izquierda = x - 2
+            x = Izquierda
+            Posicion_jugador = [x, y]
+        ##Configuración de controles con flechas
+        elif Tecla_presionada[pygame.K_UP]:
+            Arriba = y - 2
             y = Arriba #Obliga a la variable y a ser igual al valor de Arriba
             Posicion_jugador = [x, y]
             # print(y)
         elif Tecla_presionada[pygame.K_LEFT]:
-            Izquierda = x - 1
+            Izquierda = x - 2
             x = Izquierda #Obliga a la variable x a hacer igual de Izquierda
-            Posicion_jugador = [Izquierda, y]
+            Posicion_jugador = [x, y]
             # print(x)
         elif Tecla_presionada[pygame.K_RIGHT]:
-            Derecha = x + 1
+            Derecha = x + 2
             x = Derecha #Obliga a la variable x a hacer igual de Derecha
-            Posicion_jugador = [Derecha, y]
+            Posicion_jugador = [x, y]
             # print(x)
         elif Tecla_presionada[pygame.K_DOWN]:
-            Abajo = y + 1
+            Abajo = y + 2
             y = Abajo #Obliga a la variable y a ser igual al valor de Abajo
-            Posicion_jugador = [x, Abajo]
+            Posicion_jugador = [x, y]
             # print(y)
+        ###Configuración con controles WSAD
+        elif Tecla_presionada[pygame.K_w]:
+            Arriba = y - 2
+            y = Arriba #Obliga a la variable y a ser igual al valor de Arriba
+            Posicion_jugador = [x, y]
+            # print(y)
+        elif Tecla_presionada[pygame.K_a]:
+            Izquierda = x - 2
+            x = Izquierda #Obliga a la variable x a hacer igual de Izquierda
+            Posicion_jugador = [x, y]
+            # print(x)
+        elif Tecla_presionada[pygame.K_d]:
+            Derecha = x + 2
+            x = Derecha #Obliga a la variable x a hacer igual de Derecha
+            Posicion_jugador = [x, y]
+            # print(x)
+        elif Tecla_presionada[pygame.K_s]:
+            Abajo = y + 2
+            y = Abajo #Obliga a la variable y a ser igual al valor de Abajo
+            Posicion_jugador = [x, y]
+            # print(y)
+    ### Situación de correr ###
+    if Tecla_presionada[pygame.K_LSHIFT] or Tecla_presionada[pygame.K_RSHIFT]:  #Opción de correr con el L_Shift o el shift izquierdo 
+        Correr = bool; Correr = True
+        while Correr == True: #Cuando el shift lo hayan presionado, correra la distancia 1 * 1.7
+            if Tecla_presionada[pygame.K_UP]:
+                Arriba = y - 2* Velocidad_jugadory #Agrega una variación mayor para tener velocidad
+                y = Arriba #Obliga a la variable y a ser igual al valor de Arriba
+                Posicion_jugador = [x, y]
+                # print(y)
+            elif Tecla_presionada[pygame.K_LEFT]:
+                Izquierda = x - 2* Velocidad_jugadorx #Agrega una variación mayor para tener velocidad
+                x = Izquierda #Obliga a la variable x a hacer igual de Izquierda
+                Posicion_jugador = [x, y]
+                # print(x)
+            elif Tecla_presionada[pygame.K_RIGHT]:
+                Derecha = x + 2* Velocidad_jugadorx #Agrega una variación mayor para tener velocidad
+                x = Derecha #Obliga a la variable x a hacer igual de Derecha
+                Posicion_jugador = [x, y]
+                # print(x)
+            elif Tecla_presionada[pygame.K_DOWN]:
+                Abajo = y + 2* Velocidad_jugadory #Agrega una variación mayor para tener velocidad
+                y = Abajo #Obliga a la variable y a ser igual al valor de Abajo
+                Posicion_jugador = [x, y]
+                # print(y)
+            if Tecla_presionada[pygame.K_w]:
+                Arriba = y - 2* Velocidad_jugadory #Agrega una variación mayor para tener velocidad
+                y = Arriba #Obliga a la variable y a ser igual al valor de Arriba
+                Posicion_jugador = [x, y]
+                # print(y)
+            elif Tecla_presionada[pygame.K_a]:
+                Izquierda = x - 2* Velocidad_jugadorx #Agrega una variación mayor para tener velocidad
+                x = Izquierda #Obliga a la variable x a hacer igual de Izquierda
+                Posicion_jugador = [x, y]
+                # print(x)
+            elif Tecla_presionada[pygame.K_d]:
+                Derecha = x + 2* Velocidad_jugadorx #Agrega una variación mayor para tener velocidad
+                x = Derecha #Obliga a la variable x a hacer igual de Derecha
+                Posicion_jugador = [x, y]
+                # print(x)
+            elif Tecla_presionada[pygame.K_s]:
+                Abajo = y + 2* Velocidad_jugadory #Agrega una variación mayor para tener velocidad
+                y = Abajo #Obliga a la variable y a ser igual al valor de Abajo
+                Posicion_jugador = [x, y]
+                # print(y)
+            if Tecla_presionada[pygame.K_LSHIFT] or Tecla_presionada[pygame.K_RSHIFT]: #Cuando se deja de presionar el shift izquierdo, el L-shift
+                Correr = False
     ##### Eventos que incluyan acciones de condiciones y/o correciones, arriba de acá ######
+
     #Configuración de la ventana
     Ventana_principal.fill(BLANCO) #Color de fondo
     Ventana_principal.blit(Imagen_jugador, Posicion_jugador) #Imagen del objeto y su posicion para siempre
     pygame.display.flip()
     # print("Ventana actualizada") #Validación 6#
-
     ### Todo el código de sprites, dibujos y música deben ir encima del comentario ###
     
     ### Limita a 60 fotogramas por segundo (Imágenes cargadas y pasadas por segundo) ###
